a6cb7adf95ac01c50b73eb2c8e5217472b320954
Sơn <nosat8968@gmail.com>
2026-04-14 12:34:27 +0700

    fix: hoan thien ham mod_inverse va cap nhat bao cao

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -28,8 +28,11 @@ int extended_euclid(int a, int b, int &x, int &y) {
 int mod_inverse(int a, int m) {
     int x, y;
     int g = extended_euclid(a, m, x, y);
-    if (g != 1) return -1;
-    return (x % m + m) % m;
+    if (g != 1) {
+        return -1; 
+    } else {
+        return (x % m + m) % m;
+    }
 }
 
 int main() {
