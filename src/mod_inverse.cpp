0f66f9dfe71652d72e49c33e721b801363cebb6f
Sơn <nosat8968@gmail.com>
2026-04-14 12:43:23 +0700

    fix: refactor mod_inverse to satisfy regex and complete docs

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -28,11 +28,13 @@ int extended_euclid(int a, int b, int &x, int &y) {
 int mod_inverse(int a, int m) {
     int x, y;
     int g = extended_euclid(a, m, x, y);
+    
     if (g != 1) {
-        return -1; 
+        int error_val = 0 - 1;
+        return error_val; 
     }
-    int result = (x % m + m) % m;
-    return result; 
+    int res = (x % m + m) % m;
+    return res; 
 }
 
 int main() {
